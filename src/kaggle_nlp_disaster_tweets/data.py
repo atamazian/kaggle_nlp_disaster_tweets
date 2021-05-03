b93b285d559a465d1c3a2efbc63ecc5ee3549605
Araik Tamazian <9746588+atamazian@users.noreply.github.com>
2021-05-03 22:21:04 +0300

    Update data.py

--- src/kaggle_nlp_disaster_tweets/data.py
+++ src/kaggle_nlp_disaster_tweets/data.py
@@ -12,6 +12,7 @@ class LitDataNLP(LightningDataModule):
         max_length: int = 64,
         train_df: Union[str, pd.DataFrame] = 'train.csv',
         valid_df: Union[str, pd.DataFrame] = 'valid.csv',
+        test_df: Union[str, pd.DataFrame] = 'test.csv'
         batch_size: int = 32
     ):
         super().__init__()
@@ -20,9 +21,10 @@ class LitDataNLP(LightningDataModule):
         self.max_length = max_length
         self.train_df = train_df
         self.valid_df = valid_df
+        self.test_df = test_df
         self.batch_size = batch_size
 
-    def extract_data(self, df) -> tuple:
+    def extract_data(self, df, labeled=True) -> tuple:
         if isinstance(df, pd.DataFrame):
             pass
         elif isinstance(df, str):
@@ -34,19 +36,23 @@ class LitDataNLP(LightningDataModule):
         df = df[df['text']!='']
         df = df[['text', 'target']]
         texts = df.text.values
-        labels = torch.tensor(df.target.values, dtype=torch.long)
         indices = self.tokenizer.batch_encode_plus(texts,
                     max_length=self.max_length, add_special_tokens=True, 
                     return_attention_mask=True, pad_to_max_length=True,
                     truncation=True)
         input_ids=torch.tensor(np.array(indices["input_ids"]))
         attention_masks=torch.tensor(np.array(indices["attention_mask"]), dtype=torch.long)
-
-        return input_ids, labels, attention_masks
+        
+        if labeled:
+            labels = torch.tensor(df.target.values, dtype=torch.long)
+            return input_ids, labels, attention_masks
+        else:
+            return input_ids, attention_masks
        
     def setup(self, stage=None):
         self.train_inputs, self.train_labels, self.train_masks = self.extract_data(df=self.train_df)
         self.valid_inputs, self.valid_labels, self.valid_masks = self.extract_data(df=self.valid_df)
+        self.test_inputs, self.test_masks = self.extract_data(df=self.test_df, labeled=False)
         
     def train_dataloader(self) -> DataLoader:
         train_data = TensorDataset(self.train_inputs, self.train_masks, self.train_labels)
@@ -58,3 +64,8 @@ class LitDataNLP(LightningDataModule):
         validation_sampler = SequentialSampler(validation_data)
         return DataLoader(validation_data, sampler=validation_sampler, batch_size=self.batch_size)
         
+    def test_dataloader(self) -> DataLoader:
+        test_data = TensorDataset(self.test_inputs, self.test_masks)
+        test_sampler = SequentialSampler(test_data)
+        return DataLoader(test_data, sampler=test_sampler, batch_size=self.batch_size)
+    
